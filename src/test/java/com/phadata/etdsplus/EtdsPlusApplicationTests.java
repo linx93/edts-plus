51c1927ca92c46d3f4ef48db90143ecfae7c75a1
xionglin <xionglin@fe-cred.com>
2021-11-17 14:17:30 +0800

    init etds-plus

---
+++ src/test/java/com/phadata/etdsplus/EtdsPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phadata.etdsplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtdsPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
