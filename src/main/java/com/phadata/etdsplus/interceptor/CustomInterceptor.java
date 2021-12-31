945ad1a23a3860d2f19dc0354312a6c4f10bba18
xionglin <xionglin@fe-cred.com>
2021-12-31 10:28:02 +0800

    1. 对定制层的拦截器增加校验ETDS过期逻辑

--- src/main/java/com/phadata/etdsplus/interceptor/CustomInterceptor.java
+++ src/main/java/com/phadata/etdsplus/interceptor/CustomInterceptor.java
@@ -1,14 +1,18 @@
 package com.phadata.etdsplus.interceptor;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.phadata.etdsplus.entity.po.Etds;
 import com.phadata.etdsplus.exception.BussinessException;
 import com.phadata.etdsplus.service.DataSwitchService;
+import com.phadata.etdsplus.service.EtdsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
 
 
 /**
@@ -20,9 +24,11 @@ import javax.servlet.http.HttpServletResponse;
 public class CustomInterceptor implements HandlerInterceptor {
 
     private final DataSwitchService dataSwitchService;
+    private final EtdsService etdsService;
 
-    public CustomInterceptor(DataSwitchService dataSwitchService) {
+    public CustomInterceptor(DataSwitchService dataSwitchService, EtdsService etdsService) {
         this.dataSwitchService = dataSwitchService;
+        this.etdsService = etdsService;
     }
 
     @Override
@@ -36,6 +42,18 @@ public class CustomInterceptor implements HandlerInterceptor {
         boolean flag = dataSwitchService.findFlag();
         if (!flag) {
             throw new BussinessException("TDaaS已关闭了ETDS的工作");
+        }
+        // 检查etds是否已经过期
+        Etds one;
+        try {
+            one = etdsService.getOne(new QueryWrapper<>());
+        } catch (Exception e) {
+            throw new BussinessException("存在多条ETDS信息异常");
+        }
+        Long licenseExpirationTime = one.getLicenseExpirationTime();
+        //当前时间大于过期时间代表已经过期
+        if (Instant.now().getEpochSecond() - licenseExpirationTime >= 0) {
+            throw new BussinessException("ETDS已过期，请联系管理员");
         }
         return true;
     }
