ac837004dca3f6710b3025464c540f745f5e0e1c
xionglin <xionglin@fe-cred.com>
2022-01-05 12:11:48 +0800

    1. 优化ETDS请求鉴权中心获取token的逻辑，增加缓存

--- src/test/java/com/phadata/etdsplus/GetTokenTest.java
+++ src/test/java/com/phadata/etdsplus/GetTokenTest.java
@@ -1,17 +1,34 @@
 package com.phadata.etdsplus;
 
-import cn.hutool.http.HttpRequest;
+import com.phadata.etdsplus.service.impl.EtdsServiceImpl;
 import org.junit.jupiter.api.Test;
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
 /**
  * 到鉴权中心获取token测试
+ *
  * @author: lin
  * @since 2021-11-17 19:57
  */
+@SpringBootTest
 public class GetTokenTest {
 
+    @Autowired
+    private EtdsServiceImpl etdsServiceImpl;
 
+    /**
+     * 测试去获取鉴权中心获取token
+     */
+    @Test
+    void reqAuthToken() {
+        int num = 0;
+        while (true) {
+            if (num > 100) break;
+            String token = etdsServiceImpl.getToken();
+            System.out.println(token);
+            num++;
+        }
+    }
 }
